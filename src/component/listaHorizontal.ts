f6c37db377ef46cfa903298dde797986825c331e
evandrouzeda <evandrouzeda@gmail.com>
2023-10-17 12:40:53 +0000

    melhorando layout

--- src/component/listaHorizontal.ts
+++ src/component/listaHorizontal.ts
@@ -3,6 +3,7 @@ import Component from ".";
 import App from "../app";
 import { Adapter } from "./adapter/lib";
 import CardSimple from "./cardSimple";
+import style from "./lista.module.css";
 
 export default class ListaHorizontal extends Component {
     main: Zeyo = Z("div");
@@ -12,11 +13,11 @@ export default class ListaHorizontal extends Component {
         this.card = card
     }
     async create(obj: {adapter: Adapter, title: string, list: {title: string, description: string}[]}): Promise<Zeyo> {
-        return this.main = Z("div").children(
+        return this.main = Z("div").class("d-grid", "gap-m", style.lista ).children(
             Z("h2").text(obj.title),
             ...(await Promise.all(obj.list.map(async e => {
                 const [es, c] = new this.card(this.app, obj.adapter).watch(e);
-                return await c.create(es)
+                return (await c.create(es)).class(style.card)
             })))
         )
     }
