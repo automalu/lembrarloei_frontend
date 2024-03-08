6a503d3bc8039470e204bfa376162dcd30e5d506
evandrouzeda <evandrouzeda@gmail.com>
2024-03-08 19:05:22 +0000

    adicionado data para pegar registros

--- src/component/dashboard/acesso/index.ts
+++ src/component/dashboard/acesso/index.ts
@@ -1,24 +1,20 @@
-import Z, { Zeyo } from "zeyo"
+import Z, { Zeyo, ZeyoAs } from "zeyo"
 import App from "../../../app"
 import Card from "../../card"
 
 export default class Acesso {
     app: App
+    counter: ZeyoAs<"p">
+    element: Zeyo
     constructor(app: App) {
         this.app = app
+        this.element = Card(
+            Z("h3").text("Acessos"),
+            this.counter = Z("p")
+        ).class("acesso")
+        this.setData([])
     }
-    create(hostname: string, component?: any): Zeyo {
-        return Card(
-            Z("h3").text("Acessos")
-        ).class("acesso").object(async o => {
-            const [result, err] = await this.app.repository.findMany("Registros", {hostname})
-            if(err) return console.error(result);
-            
-            result.forEach(element => {
-                if(Object.prototype.hasOwnProperty.call(element, "date"))
-                    element.date = new Date(element.date)
-            });
-            o.children(Z("p").text(result.length.toString()))
-        })
+    setData(result: any[]) {
+        this.counter.text(result.length.toString())
     };
 }
--- src/states/estabelecimento/componente.ts
+++ src/states/estabelecimento/componente.ts
@@ -1,4 +1,4 @@
-import Z from "zeyo";
+import Z, { ZeyoAs } from "zeyo";
 import App from "../../app";
 import Card from "../../component/card";
 import Acesso from "../../component/dashboard/acesso";
@@ -13,20 +13,33 @@ export default function Componente<Base extends StateBaseConstructor>(base: Base
                 await this.waitsetup(app)
                 const hostname = app.session.estabelecimento ? app.session.estabelecimento.hostname : ""
                 o.children(
-                    Z("div").class(dashboard["dashboard"]).children(
-                        //seria interessante a view do componente se inscrever no componente dai quando ele alterar então alteraria 
-                        Title("Aplicativo").class(dashboard["full"]),
-                        new Acesso(app).create(hostname),
-                        /* Card(
-                            Z("h3").text("Tempo no aplicativo"),
-                            Z("p").text("2h 21min")
-                        ).class(dashboard.half), */
-                        Card(
-                            Z("h3").text("Itens mais acessados"),
-                            Z("p").text("2h 21min")
-                        ).class(dashboard.full),
-                        new Grafico(app, hostname).element.class(dashboard.full),
-                    ),
+                    Z("div").class(dashboard["dashboard"]).object(async o => {
+                        const grafico = new Grafico(app);
+                        const acesso = new Acesso(app)
+                        async function setByDate(o: ZeyoAs<"input">) {
+                            const date = new Date()
+                            const [year, month, day] = o.element.value.split("-").map(s => Number(s));
+                            date.setFullYear(year, month - 1, day);
+                            date.setHours(0, 0, 0, 0);
+                            const [result, err] = await app.repository.findMany("Registros", { hostname, date: { "$gt": date.toISOString(), "$lt": date.setDate(date.getDate() + 1) } });
+                            grafico.setData(result)
+                            acesso.setData(result)
+                        }
+                        o.children(
+                            //seria interessante a view do componente se inscrever no componente dai quando ele alterar então alteraria 
+                            Title("Aplicativo").class(dashboard["full"]),
+                            Z("input").set("type", "date").class(dashboard["full"]).on("input", setByDate).object(o => {
+                                o.element.value = new Date().toISOString().substring(0, 10);
+                                setByDate(o)
+                            }),
+                            acesso.element.class(dashboard.half),
+                            grafico.element.class(dashboard.full),
+                            /* Card(
+                                Z("h3").text("Itens mais acessados"),
+                                Z("p").text("2h 21min")
+                            ).class(dashboard.full), */
+                        )
+                    }),
                     Z("div").class(dashboard["dashboard"]).children(
                         Title("Pedidos").class(dashboard["full"]),
                     ),
