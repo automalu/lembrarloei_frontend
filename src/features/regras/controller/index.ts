7a28df669cc7526f6a8b66bdea939cc1f8bfa416
evandrouzeda <evandrouzeda@gmail.com>
2024-03-19 20:29:40 +0000

    criando regras

--- src/features/regras/controller/index.ts
+++ src/features/regras/controller/index.ts
@@ -1,4 +1,3 @@
-import Z, { Zeyo } from 'zeyo';
 import Snackbar from '../../../component/snackbar';
 import Text from '../../../component/text';
 import WaitText from '../../../component/text/waitText';
@@ -11,13 +10,14 @@ export default class CreateRegras extends Controller {
         console.log('entrou no create regras');
         console.log(form);
         const regra = Object.assign({
-            parent: form.model._id
+            parent: form.model._id,
+            estabelecimento: form.model.estabelecimento
         }, form.data)
         console.log(regra);
         Modal.setMessage(WaitText("h2", "Criando Regra"))
         const [result, err] = await this.app.repository.create("Regras", regra)
-        console.log(result, err)
         Modal.back()
+        console.log(result, err)
         Snackbar(this.app, Text('h3', "Criado com sucesso 😎"))
     }
 }
